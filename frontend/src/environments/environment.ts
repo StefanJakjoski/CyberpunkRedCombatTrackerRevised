96c2b2ffb7d8f6730fa04445dbf4a4c505395987
StefanJakjoski <stefan.jakjoski@gmail.com>
2026-02-19 00:01:13 +0100

    hosting additions

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api'  // IMPLEMENT THIS LATER
+  apiUrl: 'https://cyberpunkredcombattrackerbackend.onrender.com/api'  // IMPLEMENT THIS LATER
 };
